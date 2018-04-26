3931fdb4442435b94b153e561dc8f88e4e90bc0e
Peachiia <pk.destinazione@gmail.com>
2018-04-26 21:29:08 +0700

    Add getResistance() with Demo

--- open_heatpad_controller.ino
+++ open_heatpad_controller.ino
@@ -1,13 +1,43 @@
+#define THERMISTER_PIN A0
+#define THERMISTER_REF_RESISTER 9960.0
+#define ADC_MAX 1023
+
+
 void setup()
 {
-
-  /* add setup code here */
-
+	Serial.begin(115200);
+	pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
+	blink_blocking(LED_BUILTIN, 100);
+	blink_blocking(LED_BUILTIN, 100);
+	blink_blocking(LED_BUILTIN, 100);
 
-  /* add main program code here */
+	Serial.print("Resistance: ");
+	Serial.print(getResistance(THERMISTER_REF_RESISTER, analogRead(THERMISTER_PIN)));
+	Serial.print(" Ohms");
 
+	Serial.print(" (");
+	Serial.print(analogRead(THERMISTER_PIN));
+	Serial.println(")");
+	blink_blocking(LED_BUILTIN, 400);
+}
+
+void blink_blocking(int pin, int duration)
+{
+	digitalWrite(pin, HIGH);
+	delay(duration);
+	digitalWrite(pin, LOW);
+	delay(duration);
+}
+
+double getResistance(double Rref, int ADCval)
+{
+	// For Normal Measuring Circuit
+	// return Rref / ((ADC_MAX/ (double)ADCval) - 1);
+
+	// For Reverse Measuring Circuit
+	return Rref * ((ADC_MAX / ((double)ADCval)) - 1); 
 }
