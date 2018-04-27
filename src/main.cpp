31cc2d224a226411c238b35319431b168786c91e
Peachiia <pk.destinazione@gmail.com>
2018-04-28 01:43:38 +0700

    add task_Temperature()

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,22 @@
 #include <Arduino.h>
+//#define VERBOSE
+
+#define STATIC_TIMER_UPDATE timer = millis() + duration
+#define STATIC_TIMER_INIT static unsigned long STATIC_TIMER_UPDATE
+#define STATIC_TIMER_CHECK (millis() >= timer)
 
 #define THERMISTER_PIN A0
 #define THERMISTER_REF_RESISTER 9960.0
 #define ADC_MAX 1023
 
+#define TEMP_KELVIN_DIFF 273.15				// Constant Difference between Kelvin to Celcius
 #define THERMISTER_ROOM_RESISTANCE 10000    // Resistance @ Norminal  
 #define THERMISTER_ROOM_TEMP 25				// Temp @ Norminal
 #define THERMISTER_COEF_B 2988.64			// Beta Coefficient of thermistor (usually 3000-4000)
 											// 2988.64 == getBetaCoef(62, 3324.33, 5.5, 20274.66)
 
-#define TEMP_KELVIN_DIFF 273.15				// Constant Difference between Kelvin to Celcius
+double resistance = 10000.0;
+double celcius = 25.0;
 
 #define CONTROLLER_PWM_MAX 255
 #define CONTROLLER_PWM_PIN 6
@@ -24,6 +31,8 @@ double getBetaCoef(double T1, double R1, double T2, double R2);
 void initController();
 bool setController(double percentage);
 
+void task_Temperature(int duration);
+
 void setup()
 {
 	pinMode(LED_BUILTIN, OUTPUT);
@@ -35,25 +44,26 @@ void setup()
 
 void loop()
 {
-	blink_blocking(LED_BUILTIN, 100);
-	blink_blocking(LED_BUILTIN, 100);
-	blink_blocking(LED_BUILTIN, 100);
-
-	double resistance = getResistance(THERMISTER_REF_RESISTER, analogRead(THERMISTER_PIN));
-	double celcius = toCelcius(resistance);
-
-	Serial.print("Resistance: ");
-	Serial.print(resistance);
-	Serial.print(" Ohms");
-
-	Serial.print(" (");
-	Serial.print(analogRead(THERMISTER_PIN));
-	Serial.print(") -> ");
-
+	task_Temperature(500);
+	task_Temperature(500);
+/*
 	Serial.print(celcius);
-	Serial.println(" C");
+	Serial.print(" ");
+	Serial.print(38);
+	Serial.println();
+	blink_blocking(LED_BUILTIN, 5);
+	*/
+}
 
-	blink_blocking(LED_BUILTIN, 400);
+void task_Temperature(int duration)
+{
+	STATIC_TIMER_INIT;
+	if (STATIC_TIMER_CHECK) {
+		resistance = getResistance(THERMISTER_REF_RESISTER, analogRead(THERMISTER_PIN));
+		celcius = toCelcius(resistance);
+		Serial.println("Task");
+		STATIC_TIMER_UPDATE;
+	}
 }
 
 void blink_blocking(int pin, int duration)
@@ -80,7 +90,7 @@ double toCelcius(double resistance)
 	value = resistance / THERMISTER_ROOM_RESISTANCE;    // (R/Ro)
 	value = log(value);									// ln(R/Ro)
 	value /= THERMISTER_COEF_B;							// 1/B * ln(R/Ro)
-	value += 1.0 / (THERMISTER_ROOM_TEMP + 273.15);		// + (1/To)
+	value += 1.0 / (THERMISTER_ROOM_TEMP + TEMP_KELVIN_DIFF);		// + (1/To)
 	value = 1.0 / value;								// Invert
 	value -= TEMP_KELVIN_DIFF;							// Convert to C
 	return value;
@@ -99,7 +109,9 @@ double getBetaCoef(double T1, double R1, double T2, double R2)
 
 void initController()
 {
-	Serial.println("Init Controller");
+	#ifdef VERBOSE
+		Serial.println("Init Controller");
+	#endif
 	pinMode(CONTROLLER_PWM_PIN, OUTPUT);
 	pinMode(CONTROLLER_INA_PIN, OUTPUT);
 	pinMode(CONTROLLER_INB_PIN, OUTPUT);
@@ -112,10 +124,12 @@ void initController()
 bool setController(double percentage)
 {
 	int pwm = (int)(CONTROLLER_PWM_MAX * (percentage/100));
-	Serial.print("setController: ");
-	Serial.print(percentage);
-	Serial.print("% -> PWM : ");
-	Serial.println(pwm);
+	#ifdef VERBOSE
+		Serial.print("setController: ");
+		Serial.print(percentage);
+		Serial.print("% -> PWM : ");
+		Serial.println(pwm);
+	#endif
 	if (pwm >= 0 && pwm <= CONTROLLER_PWM_MAX)
 	{
 		analogWrite(CONTROLLER_PWM_PIN, pwm);
