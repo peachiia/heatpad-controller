3b7e721064427714dcaeb4d1ae9fbc89501c241a
Peachiia <pk.destinazione@gmail.com>
2018-05-03 22:43:24 +0700

    struct profile_storage  implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,53 @@
 #include <Arduino.h>
+#include <EEPROM.h>
 #include <stdio.h>
 //#define VERBOSE
 
-#pragma region TASK_TIMER
+struct profile_storage
+{
+	uint8_t THERMISTER_PIN = 0;
+	double THERMISTER_REF_RESISTER = 9960.0;
+	int ADC_MAX = 1023;
+
+	double THERMISTER_ROOM_RESISTANCE = 10000;    	// Resistance @ Norminal  
+	double THERMISTER_ROOM_TEMP = 25;				// Temp @ Norminal
+	double THERMISTER_COEF_B = 2988.64;				// Beta Coefficient of thermistor
+													// 2988.64 == getBetaCoef(62, 3324.33, 5.5, 20274.66)
+
+	int DENOISE_ORDER = 1;
+	double DENOISE_DIFF_LIMIT = 0.02;
+
+	int CONTROLLER_PWM_MAX = 255;
+	uint8_t CONTROLLER_PWM_PIN = 6;
+	uint8_t CONTROLLER_INA_PIN = 7;
+	uint8_t CONTROLLER_INB_PIN = 8;
+
+	double kP = 500.0; 
+	double kI = 0;
+	double kD = 0;
+
+	double setpointTemp = 40.0;
+} profile;
+
+#pragma region TASK
 	#define STATIC_TIMER_UPDATE timer = millis() + duration					// Reschedule for next calling
 	#define STATIC_TIMER_INIT static unsigned long STATIC_TIMER_UPDATE		// Schedule Variable Declaration
 	#define STATIC_TIMER_CHECK (millis() >= timer)							// Schedule Checking
+
+	void task_run();
+	void task_stop();
+	bool task_isRunning();
+
+	bool isControlTaskEnabled = false;
 #pragma endregion
 
 #pragma region THERMISTER
-	#define THERMISTER_PIN A0
-	#define THERMISTER_REF_RESISTER 9960.0
-	#define ADC_MAX 1023
-
 	#define TEMP_KELVIN_DIFF 273.15				// Constant Difference between Kelvin to Celcius
-	#define THERMISTER_ROOM_RESISTANCE 10000    // Resistance @ Norminal  
-	#define THERMISTER_ROOM_TEMP 25				// Temp @ Norminal
-	#define THERMISTER_COEF_B 2988.64			// Beta Coefficient of thermistor (usually 3000-4000)
-												// 2988.64 == getBetaCoef(62, 3324.33, 5.5, 20274.66)
 
 	double resistance = 10000.0;
 	double noisyTemp = 25.0;
 	double denoisedTemp = 25.0;
 	double currentTemp = 25.0;
-
-	#define DENOISE_ORDER 1
-	#define DENOISE_DIFF_LIMIT 0.02
 
 	double getResistance(double Rref, int ADCval);
 	double toCelcius(double resistance);
@@ -34,30 +56,34 @@
 #pragma endregion
 
 #pragma region CONTROLLER
-	#define CONTROLLER_PWM_MAX 255
-	#define CONTROLLER_PWM_PIN 6
-	#define CONTROLLER_INA_PIN 7
-	#define CONTROLLER_INB_PIN 8
-
-	bool isControlTaskEnabled = false;
-
 	void initController();
 	bool setController(double percentage);
 #pragma endregion
 
 #pragma region PID
-	double kP = 500; // 200
-	double kI = 0; // 2
-	double kD = 0; //5
 	double valP, valI, valD, valPID;
 
 	double previousTemp = 25.0;
-	double setpointTemp = 40.0;
 	double errorTemp = 0;
 
 	double deltaTime = 0;
 	double controlPWM = 0;
 #pragma endregion
+
+#pragma region TERMINAL
+	#define COMMAND_MAX_LENGTH 50
+	char bufferString[COMMAND_MAX_LENGTH];
+	int bufferLength = 0;
+	char bufferChar;
+
+	char command[COMMAND_MAX_LENGTH];
+	int commandLength = 0;
+	bool flagExecRequest = false;
+	bool flagTerminalResume = true;
+
+	void init_Terminal();
+#pragma endregion
+
 
 void task_Temperature(long duration);
 void task_PID(long duration);
@@ -66,21 +92,22 @@ void task_Plot(long duration);
 void task_Terminal(long duration);
 void task_Exec();
 
-void task_run();
-void task_stop();
-bool task_isRunning();
-
 bool isMatch(char *p,  char *keyword);
 void command_help();
 void command_info();
 void command_run();
 void command_stop();
+void command_default();
+void command_show();
+void command_load();
+void command_save();
+void command_set();
 
 void setup()
 {
 	pinMode(LED_BUILTIN, OUTPUT);
-	Serial.begin(115200);
-	
+
+	init_Terminal();
 	task_run();
 }
 
@@ -91,7 +118,7 @@ void loop()
 		task_Temperature(10);
 		task_PID(50);
 		task_Controller(50);
-		//task_Plot(20);
+		task_Plot(20);
 	}
 	task_Terminal(50);
 	task_Exec();
@@ -100,7 +127,10 @@ void loop()
 
 void init_Temperature()
 {
-
+	resistance = 10000.0;
+	noisyTemp = 25.0;
+	denoisedTemp = 25.0;
+	currentTemp = 25.0;
 }
 
 
@@ -108,7 +138,7 @@ void task_Temperature(long duration)
 {
 	STATIC_TIMER_INIT;
 	if (STATIC_TIMER_CHECK) {
-		resistance = getResistance(THERMISTER_REF_RESISTER, analogRead(THERMISTER_PIN));
+		resistance = getResistance(profile.THERMISTER_REF_RESISTER, analogRead(profile.THERMISTER_PIN));
 		noisyTemp = toCelcius(resistance);
 		denoisedTemp = getDenoisedData(noisyTemp);
 		currentTemp = denoisedTemp;
@@ -119,7 +149,13 @@ void task_Temperature(long duration)
 
 void init_PID()
 {
+	valPID = 0;
 
+	previousTemp = 25.0;
+	errorTemp = 0;
+
+	deltaTime = 0;
+	controlPWM = 0;
 }
 
 
@@ -128,13 +164,13 @@ void task_PID(long duration)
 	STATIC_TIMER_INIT;
 	if (STATIC_TIMER_CHECK) {
 		double previousTime = millis();
-		errorTemp = setpointTemp - currentTemp;
+		errorTemp = profile.setpointTemp - currentTemp;
 		deltaTime = (millis() - previousTime) / 1000;
 		previousTime = millis();
 
-		valP = errorTemp * kP;
-		valI = valI + ((errorTemp * kI) * deltaTime);
-		valD = (previousTemp - currentTemp) * kD;
+		valP = errorTemp * profile.kP;
+		valI = valI + ((errorTemp * profile.kI) * deltaTime);
+		valD = (previousTemp - currentTemp) * profile.kD;
 		valPID = valP + valI + valD;
 
 		STATIC_TIMER_UPDATE;
@@ -147,13 +183,13 @@ void init_Controller()
 	#ifdef VERBOSE
 		Serial.println("Init Controller");
 	#endif
-	pinMode(CONTROLLER_PWM_PIN, OUTPUT);
-	pinMode(CONTROLLER_INA_PIN, OUTPUT);
-	pinMode(CONTROLLER_INB_PIN, OUTPUT);
+	pinMode(profile.CONTROLLER_PWM_PIN, OUTPUT);
+	pinMode(profile.CONTROLLER_INA_PIN, OUTPUT);
+	pinMode(profile.CONTROLLER_INB_PIN, OUTPUT);
 
-	digitalWrite(CONTROLLER_INA_PIN, HIGH);
-	digitalWrite(CONTROLLER_INB_PIN, LOW);
-	analogWrite(CONTROLLER_PWM_PIN, 0);
+	digitalWrite(profile.CONTROLLER_INA_PIN, HIGH);
+	digitalWrite(profile.CONTROLLER_INB_PIN, LOW);
+	analogWrite(profile.CONTROLLER_PWM_PIN, 0);
 
 	setController(0);
 }
@@ -192,7 +228,7 @@ void task_Plot(long duration)
 		// Serial.print(" ");
 		Serial.print(denoisedTemp);
 		Serial.print(" ");
-		Serial.print(setpointTemp);
+		Serial.print(profile.setpointTemp);
 		// Serial.print(" // ");
 		// Serial.print(valPID);
 
@@ -200,7 +236,7 @@ void task_Plot(long duration)
 		STATIC_TIMER_UPDATE;
 	}
 
-	if (abs(currentTemp-setpointTemp) < 0.5) {
+	if (abs(currentTemp-profile.setpointTemp) < 0.5) {
 		digitalWrite(LED_BUILTIN, HIGH);
 	}
 	else {
@@ -215,7 +251,7 @@ double getResistance(double Rref, int ADCval)
 	// return Rref / ((ADC_MAX/ (double)ADCval) - 1);
 
 	// For Reverse Measuring Circuit
-	return Rref * ((ADC_MAX / ((double)ADCval)) - 1); 
+	return Rref * ((profile.ADC_MAX / ((double)ADCval)) - 1); 
 }
 
 
@@ -224,18 +260,18 @@ double getDenoisedData(double data)
 	static double buffer = data;
 	static double prev = data;
 
-	if((data - prev) > DENOISE_DIFF_LIMIT) {
-		prev += DENOISE_DIFF_LIMIT;
+	if((data - prev) > profile.DENOISE_DIFF_LIMIT) {
+		prev += profile.DENOISE_DIFF_LIMIT;
 		data = prev ;
 	}
-	else if ((prev - data) > DENOISE_DIFF_LIMIT) {
-		prev -= DENOISE_DIFF_LIMIT;
+	else if ((prev - data) > profile.DENOISE_DIFF_LIMIT) {
+		prev -= profile.DENOISE_DIFF_LIMIT;
 		data = prev;
 	} 
 	else {
 		prev = data;
 	}
-	buffer = (data + (buffer * (DENOISE_ORDER - 1))) / DENOISE_ORDER;
+	buffer = (data + (buffer * (profile.DENOISE_ORDER - 1))) / profile.DENOISE_ORDER;
 	return buffer;
 }
 
@@ -244,10 +280,10 @@ double toCelcius(double resistance)
 {
 	// Steinhart-Hart eq. (NTC - Beta method)
 	double value = 0;
-	value = resistance / THERMISTER_ROOM_RESISTANCE;    // (R/Ro)
+	value = resistance / profile.THERMISTER_ROOM_RESISTANCE;    // (R/Ro)
 	value = log(value);									// ln(R/Ro)
-	value /= THERMISTER_COEF_B;							// 1/B * ln(R/Ro)
-	value += 1.0 / (THERMISTER_ROOM_TEMP + TEMP_KELVIN_DIFF);		// + (1/To)
+	value /= profile.THERMISTER_COEF_B;							// 1/B * ln(R/Ro)
+	value += 1.0 / (profile.THERMISTER_ROOM_TEMP + TEMP_KELVIN_DIFF);		// + (1/To)
 	value = 1.0 / value;								// Invert
 	value -= TEMP_KELVIN_DIFF;							// Convert to C
 	return value;
@@ -268,15 +304,15 @@ double getBetaCoef(double T1, double R1, double T2, double R2)
 
 bool setController(double percentage)
 {
-	int pwm = (int)(CONTROLLER_PWM_MAX * (percentage/100));
+	int pwm = (int)(profile.CONTROLLER_PWM_MAX * (percentage/100));
 	#ifdef VERBOSE
 		Serial.print("setController: ");
 		Serial.print(percentage);
 		Serial.print("% -> PWM : ");
 		Serial.println(pwm);
 	#endif
-	if (pwm >= 0 && pwm <= CONTROLLER_PWM_MAX) {
-		analogWrite(CONTROLLER_PWM_PIN, pwm);
+	if (pwm >= 0 && pwm <= profile.CONTROLLER_PWM_MAX) {
+		analogWrite(profile.CONTROLLER_PWM_PIN, pwm);
 		return true;
 	}
 	else {
@@ -284,20 +320,10 @@ bool setController(double percentage)
 	}
 }
 
-#define COMMAND_MAX_LENGTH 50
-char bufferString[COMMAND_MAX_LENGTH];
-int bufferLength = 0;
-char bufferChar;
-
-char command[COMMAND_MAX_LENGTH];
-int commandLength = 0;
-bool flagExecRequest = false;
-bool flagTerminalResume = true;
-
 
 void init_Terminal()
 {
-
+	Serial.begin(115200);
 }
 
 
@@ -349,6 +375,7 @@ void task_Terminal(long duration)
 	}
 }
 
+
 void task_Exec()
 {
 	if (flagExecRequest) {
@@ -369,6 +396,21 @@ void task_Exec()
 		else if (isMatch(command, "stop")) {
 			command_stop();
 		}
+		else if (isMatch(command, "default")) {
+			command_default();
+		}
+		else if (isMatch(command, "show")) {
+			command_show();
+		}
+		else if (isMatch(command, "load")) {
+			command_load();
+		}
+		else if (isMatch(command, "save")) {
+			command_save();
+		}
+		else if (isMatch(command, "set")) {
+			command_set();
+		}
 		else {
 			Serial.println("Unknown Command!. Try 'help'.");
 		}
@@ -377,6 +419,7 @@ void task_Exec()
 		flagTerminalResume = true;
 	}
 }
+
 
 bool isMatch(char *p, char *keyword)
 {
@@ -390,6 +433,7 @@ bool isMatch(char *p, char *keyword)
 	return (i == len);
 }
 
+
 void command_help()
 {
 	Serial.print( F(  "\n  Open Heat-Pad Controller Terminal\n" \
@@ -397,12 +441,14 @@ void command_help()
 						"    info        : show info of system\n" \
 						"    run         : start control loop\n" \
 						"    stop        : stop control loop\n" \
-						"    get         : get value of ALL KEY\n" \
-						"    get <k>     : get value of specific key\n" \
-						"    set <k> <v> : set value to specific key\n" \
-						"    hardreset   : restore to DEFAULT parameter\n" \
+						"    showprofile : show current profile\n" \
+						"    loadprofile : load profile from Storage\n" \
+						"    saveprofile : save profile to Storage\n" \
+						"    set <k> <v> : set value to specific profile key\n" \
+						"    default     : restore profile on RAM to DEFAULT\n" \
 					));
 }
+
 
 void command_info()
 {
@@ -411,13 +457,14 @@ void command_info()
 	Serial.println(task_isRunning()? "Running":"STOP");
 
 	Serial.print(F("   - Setpoint Temp.: "));
-	Serial.println(setpointTemp);
+	Serial.println(profile.setpointTemp);
 
 	if(task_isRunning()) {
 		Serial.print(F("   - Current Temp. : "));
 		Serial.println(currentTemp);
 	}
 }
+
 
 void command_run()
 {
@@ -432,6 +479,7 @@ void command_run()
 	command_info();
 }
 
+
 void command_stop()
 {
 	Serial.print(F("Task stopping... "));
@@ -439,6 +487,8 @@ void command_stop()
 	Serial.print(F("DONE\n"));
 	command_info();
 }
+
+
 void task_run()
 {
 	isControlTaskEnabled = true;
@@ -449,13 +499,59 @@ void task_run()
 	init_Terminal();
 }
 
+
 void task_stop()
 {
 	isControlTaskEnabled = false;
 	setController(0);
 }
 
+
 bool task_isRunning()
 {
 	return isControlTaskEnabled;
+}
+
+
+void command_default()
+{
+	if (task_isRunning()) {
+		Serial.print(F("FAIL: Use 'stop' to stop control task first."));
+	}
+	else {
+		Serial.print(F("Set Default to Current Profile... "));
+		struct profile_storage default_profile;
+		profile = default_profile;
+		Serial.print(F("DONE\n"));
+
+		command_show();
+	}
+}
+
+
+void command_show()
+{
+
+}
+
+
+void command_load()
+{
+	Serial.print(F("Loading Profile from Storage... "));
+	EEPROM.get(0, profile);
+	Serial.print(F("DONE\n"));
+}
+
+
+void command_save()
+{
+	Serial.print(F("Saving Profile to storage... "));
+	EEPROM.put(0, profile);
+	Serial.print(F("DONE\n"));
+}
+
+
+void command_set()
+{
+
 }
